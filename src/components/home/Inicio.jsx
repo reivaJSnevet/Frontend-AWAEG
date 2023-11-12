efe4faab09516c7c5a697ee4c0b56b91c1a94c27
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2023-11-12 14:09:51 -0600

    Refactor CSS styles in Inicio.jsx and NavbarH.jsx

--- src/components/home/Inicio.jsx
+++ src/components/home/Inicio.jsx
@@ -5,19 +5,18 @@ const Inicio = () => {
   return (
     <div
       name="inicio"
-      className="w-full h-screen bg-zinc-200 flex flex-col justify-between p-4"
+      className="flex flex-col justify-between w-full h-screen p-4 bg-zinc-200"
     >
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+        <div className="flex flex-col justify-center w-full px-2 py-8 md:items-start">
           <p className="text-2xl">Escuela Guayabal</p>
           <h1 className="py-3 text-5xl font-bold">Bienvenido</h1>
           <p className="text-2xl">AWAEG</p>
           <Link to="/login">
             <button
-              className="text-black border-none bg-indigo-600 mr-4 px-8 py-3 border-indigo-600 
-                 hover:text-white rounded-md"
+              className="px-8 py-3 mr-4 text-black bg-indigo-600 border-indigo-600 border-none rounded-md hover:text-white"
             >
-              Iniciar Seción
+              Iniciar Sesión
             </button>
           </Link>
         </div>
