ece3869c447f2f0755e0bf9dfd42b3d7bfa2afcf
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-03-18 00:34:48 -0600

    Update AuthProvider import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { AuthProvider } from './context/AuthProvider'
+import { AuthProvider } from './context/AuthProvider.jsx'
 import './index.css'
 
 
